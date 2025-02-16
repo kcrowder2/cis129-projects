a76cfce3cdd6c64c7be0a907c196e3b23a8f2235
kcrowder2 <kngregory1@mail.pima.edu>
2025-02-16 16:56:30 -0700

    Updated previous commit to show short story in cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello World!')
+#Author: Trina Crowder
+#My First Python lab in my CIS129 course
+#Prints a simple short story
+print('There was once a mom who needed a vacation.')
+print('Moms don't get vacations they say.')
+print('Oh but this mom was determined to get her vacation!')
+print('She started scrolling on social medida looking at far away destinations.')
+print('Scotland is where I will go she thought to herself.')
